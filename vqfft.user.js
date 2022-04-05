591959ca8abf56189fe9a8fbc1bf2efbe8b92942
Yuhao Feng <git@yuhaofe.com>
2022-04-05 18:01:30 +0800

    Fix not working and refactor

--- vqfft.user.js
+++ vqfft.user.js
@@ -15,43 +15,76 @@
 
 (function() {
     'use strict';
-    var realOpen = window.XMLHttpRequest.prototype.open;
-    window.XMLHttpRequest.prototype.open = function() {
-        var url = arguments['1'];
-        var reg = /^https:\/\/video\.twimg\.com\/.+m3u8\?.*tag=/i;
-        if (reg.test(url)) {
-            this.addEventListener('readystatechange', function(e) {
-                if ( this.readyState === 4 ) {
-                    var originalText = e.target.responseText;
-                    var lines = originalText.split(new RegExp('\\r?\\n'));
-                    var modifiedText = lines[0] + '\r\n' 
-                                    + lines[1] + '\r\n' 
-                                    + lines[lines.length - 3] + '\r\n' 
-                                    + lines[lines.length - 2] + '\r\n';
-                    Object.defineProperty(this, 'response',     {writable: true});
-                    Object.defineProperty(this, 'responseText', {writable: true});
-                    this.response = this.responseText = modifiedText;
-                }
-            });
-        }
-        return realOpen.apply(this, arguments);
-    };
-
-    // add a mark helps identify if userscript loaded successfully
-    var disableHQ = localStorage.getItem('vqfft-disablehq');
-    if(!disableHQ) {
-        var mark = document.createElement('button');
-        mark.innerText = 'HQ';
-        mark.style = "position: fixed;right: 5px;top: 5px;color: white;border-width: 0px;border-radius: 5px;background-color: gray;opacity: 0.5;";
-        mark.onclick = function() {
-            if(confirm('Do not display HQ mark anymore?')){
-                localStorage.setItem('vqfft-disablehq', 'true');
-                mark.remove();
+    initHijack();
+    initUI();
+    
+    function initHijack() {
+        var realOpen = window.XMLHttpRequest.prototype.open;
+        window.XMLHttpRequest.prototype.open = hijackedOpen;
+        
+        function hijackedOpen() {
+            var url = arguments['1'];
+            if (isHLSPlaylist(url)) {
+                this.addEventListener('readystatechange', function(e) {
+                    if (this.readyState === 4) {
+                        var originalText = e.target.responseText;
+                        if(isMasterPlaylist(originalText)) {
+                            var modifiedText = modifyMasterPlaylist(originalText);
+                            Object.defineProperty(this, 'response', {writable: true});
+                            Object.defineProperty(this, 'responseText', {writable: true});
+                            this.response = this.responseText = modifiedText;
+                        }
+                    }
+                });
             }
+            return realOpen.apply(this, arguments);
         };
-        document.body.appendChild(mark);
-    }
 
-    console.log('[Video Quality Fixer for Twitter] loaded!');
+        function isHLSPlaylist(url) {
+            var reg = new RegExp(/^https:\/\/video\.twimg\.com\/.+m3u8?/, 'i') ;
+            return reg.test(url);
+        }
+
+        function isMasterPlaylist(text) {
+            return text.indexOf('#EXT-X-TARGETDURATION') === -1 && text.indexOf('#EXT-X-STREAM-INF') != -1;
+        }
+
+        function modifyMasterPlaylist(text) {
+            var result = text;
+            var reg = new RegExp(/^#EXT-X-STREAM-INF:.*BANDWIDTH=(\d+).*\r?\n.*$/, 'gm');
+            var stream = reg.exec(text);
+            if (stream) {
+                var globalTags = text.substring(0, stream.index);
+
+                // find max bitrate media playlist
+                var maxBitrateStream = stream;
+                while((stream = reg.exec(text)) != null){
+                    if (parseInt(stream[1]) > parseInt(maxBitrateStream[1])) {
+                        maxBitrateStream = stream;
+                    }
+                }
+    
+                result = globalTags + maxBitrateStream[0];
+            }
+            return result;
+        }
+    }
+    
+    function initUI() {
+        // add a mark helps identify if userscript loaded successfully
+        var disableHQ = localStorage.getItem('vqfft-disablehq');
+        if(!disableHQ) {
+            var mark = document.createElement('button');
+            mark.innerText = 'HQ';
+            mark.style = "position: fixed;right: 5px;top: 5px;color: white;border-width: 0px;border-radius: 5px;background-color: gray;opacity: 0.5;";
+            mark.onclick = function() {
+                if(confirm('Do not display HQ mark anymore?')){
+                    localStorage.setItem('vqfft-disablehq', 'true');
+                    mark.remove();
+                }
+            };
+            document.body.appendChild(mark);
+        }
+    }
 })();
 
